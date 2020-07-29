9e18b5b4caf9a16f7ced169ff6ed7aacc4e13be1
15016665135 <358422661@qq.com>
2020-07-29 10:03:51 +0800

    修改日志搜索方式

--- miya_monkey/miya_automonkey.py
+++ miya_monkey/miya_automonkey.py
@@ -355,5 +355,5 @@ if __name__ == '__main__':
     # parser.add_argument('-c' , '--channel-config', dest = 'channel_config' , help='channel_config pass to VasDolly')
     args = parser.parse_args()
     args.apkurl = 'https://ios.build.miya.chat/static/MIYA_android/2.4.0_991/MIYA.V2.4.0.991.apk'
-    args.buildnum = 728
+    args.buildnum = 991
     main(args)
