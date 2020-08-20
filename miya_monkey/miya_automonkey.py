5c9dfddce6afac9e52e319fe0051747e00dff2ad
15016665135 <358422661@qq.com>
2020-08-20 17:51:54 +0800

    new buildnum1047

--- miya_monkey/miya_automonkey.py
+++ miya_monkey/miya_automonkey.py
@@ -357,6 +357,6 @@ if __name__ == '__main__':
     parser.add_argument('-u' , '--apk-url', dest = 'apkurl' , help='need apk file url')
     # parser.add_argument('-c' , '--channel-config', dest = 'channel_config' , help='channel_config pass to VasDolly')
     args = parser.parse_args()
-    args.apkurl = 'https://ios.build.miya.chat/static/MIYA_android/2.5.0_1046/MIYA.V2.5.0.1047.apk'
+    args.apkurl = 'https://ios.build.miya.chat/static/MIYA_android/2.5.0_1047/MIYA.V2.5.0.1047.apk'
     args.buildnum = 1047
     main(args)
